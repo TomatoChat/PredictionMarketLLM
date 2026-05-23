3b3dbc98983f5fba5c7377c413b624b7ab34f7ea
TomatoChat <ar.andrearocco@gmail.com>
2026-05-23 15:49:50 +0200

    adding settings

---
+++ settings/get_settings.py
@@ -0,0 +1,8 @@
+from functools import lru_cache
+
+from settings.Settings import Settings
+
+
+@lru_cache(maxsize=1)
+def get_settings() -> Settings:
+    return Settings()  # ty: ignore[missing-argument]
