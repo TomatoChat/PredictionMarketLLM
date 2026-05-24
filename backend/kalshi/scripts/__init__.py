da52ce3a8260678d168c4962af0bbef4f2157e82
TomatoChat <ar.andrearocco@gmail.com>
2026-05-24 23:14:54 +0200

    deleting old code

--- backend/kalshi/scripts/__init__.py
+++
@@ -1,4 +0,0 @@
-from backend.kalshi.scripts.fetch_active_markets import fetch_active_markets
-from backend.kalshi.scripts.get_markets_with_retry import get_markets_with_retry
-
-__all__ = ["fetch_active_markets", "get_markets_with_retry"]
